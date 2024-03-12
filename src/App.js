7a0c58ede6983f16eb95d92de13c7c18e54f7952
Swati-Savaliya <Swatisavaliya09@gmail.com>
2024-03-12 18:51:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './User/Component/Footer/Footer';
+import Header from './User/Component/Header/Header';
+import Home from './User/Container/Home/Home';
+
+
+function App() {
+  return (
+    <div>   
+       <Header />
+       <Home />
+       <Footer />
+        
+    </div>
+  );
+}
+
+export default App;
